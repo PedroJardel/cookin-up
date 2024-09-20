6ab82721db34f30376b492586518e348bef6a7f9
Pedro Lima <pedrozagolima@gmail.com>
2024-09-20 15:46:15 -0300

    feat: use props at create children components

--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
 import { obterCategorias } from '@/http/index';
 import type ICategoria from '@/interfaces/ICategoria';
+import CardCategoria from './CardCategoria.vue';
 
 
 export default {
@@ -11,30 +12,31 @@ export default {
   },
   async created() {
     this.categorias = await obterCategorias()
-  }
+  },
+  components: { CardCategoria }
 }
 </script>
 
 <template>
-    <section class="selecionar-ingredientes">
-        <h1 class="cabecalho titulo-ingredientes">Ingredientes</h1>
-        <p class="paragrafo-lg instrucoes">
-            Selecione abaixo os ingredientes que você quer usar nesta receita:
-        </p>
-        <ul class="categorias">
-          <li v-for="categoria in categorias" :key="categoria.nome">
-            {{ categoria.nome }}
-          </li>
-        </ul>
+  <section class="selecionar-ingredientes">
+    <h1 class="cabecalho titulo-ingredientes">Ingredientes</h1>
+    <p class="paragrafo-lg instrucoes">
+      Selecione abaixo os ingredientes que você quer usar nesta receita:
+    </p>
+    <ul class="categorias">
+      <li v-for="categoria in categorias" :key="categoria.nome">
+        <CardCategoria :categoria="categoria"/>
+      </li>
+    </ul>
 
-        <p class="paragrafo dica">
-            *Atenção: consideramos que você tem em casa sal, pimenta e água.
-        </p>
-    </section>
+    <p class="paragrafo dica">
+      *Atenção: consideramos que você tem em casa sal, pimenta e água.
+    </p>
+  </section>
 </template>
 
 <style scoped>
-    .selecionar-ingredientes {
+.selecionar-ingredientes {
   display: flex;
   flex-direction: column;
   align-items: center;
