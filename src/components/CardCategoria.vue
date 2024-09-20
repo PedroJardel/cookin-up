6ac62c7d229b549e8422e5026d4c0fd340578ed4
Pedro Lima <pedrozagolima@gmail.com>
2024-09-20 15:56:06 -0300

    feat: use PropType from vuejs and manipulation dinamic src with v-bind

--- src/components/CardCategoria.vue
+++ src/components/CardCategoria.vue
@@ -11,7 +11,19 @@ import type { PropType } from 'vue';
 </script>
 
 <template>
-    {{ categoria.imagem }}
+    <article class="categoria">
+        <header class="categoria__cabecalho">
+            <img :src="`/imagens/icones/categorias_ingredientes/${categoria.imagem}`" alt="" class="categoria__imagem">
+            <h2 class="paragrafo-lg categoria__nome">
+                <ul class="categoria__ingredientes">
+                    <li v-for="ingrediente in categoria.ingredientes" :key="ingrediente">
+                        {{ ingrediente }}
+                    </li>
+                </ul>
+                {{  categoria.nome }}
+            </h2>
+        </header>
+    </article>
     {{ categoria.nome }}
     {{ categoria.ingredientes }}
 </template>
